a67e96967c754e739be3685a13ab87966c751c55
nitrogen9 <rdxgalaxy7@gmail.com>
2018-04-07 16:47:02 +0530

    Auth done

--- app/src/main/java/com/example/denish/icampus/MainActivity.java
+++ app/src/main/java/com/example/denish/icampus/MainActivity.java
@@ -1,40 +1,116 @@
 package com.example.denish.icampus;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.Arrays;
 
 public class MainActivity extends BaseActivity {
+
+    private static final String TAG = "MainActivity";
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mRoleDBReference;
+    private ChildEventListener mChildEventListener;
+
+    private String mUsername;
+    public static final String ANONYMOUS = "anonymous";
+    public static final int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         activeToolbar(false);
 
+        mUsername = ANONYMOUS;
+        Log.d(TAG, "onCreate: starts");
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+
+        mRoleDBReference = mFirebaseDatabase.getReference().child("role");
+
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    //onSignedInInitialized(user.getDisplayName());
+                    mUsername = user.getDisplayName();
+                    Log.d(TAG, "onAuthStateChanged: user name : "+ mUsername);
+
+                } else {
+                    // User is signed out
+                    //onSignedOutCleanup();
+                    Log.d(TAG, "onAuthStateChanged: starting AuthUI");
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.sign_out_menu:
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+            Toast.makeText(this, "Signed In", Toast.LENGTH_SHORT).show();
+            }
+            else if(resultCode == RESULT_CANCELED){
+                Toast.makeText(this, "Signed In Cancelled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/denish/icampus/Model/Role.java
@@ -0,0 +1,17 @@
+package com.example.denish.icampus.Model;
+
+/**
+ * Created by denish on 7/4/18.
+ */
+
+public class Role {
+    String mail;
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
