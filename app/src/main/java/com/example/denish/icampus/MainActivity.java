53baea8d60de1e99a0a5549a650d9a6b4eeb02f2
nitrogen9 <rdxgalaxy7@gmail.com>
2018-04-07 11:55:34 +0530

    splash activity successful

--- app/src/main/java/com/example/denish/icampus/MainActivity.java
+++ app/src/main/java/com/example/denish/icampus/MainActivity.java
@@ -1,11 +1,6 @@
 package com.example.denish.icampus;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,7 +11,8 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        activeToolbar(true);
+        activeToolbar(false);
+
 
     }
 
